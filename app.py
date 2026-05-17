60176782b4cde6418eeaa161f5aca642799c1205
Sangeetha-123549 <sangeethacseaisvpcet@gmail.com>
2026-05-17 12:03:14 +0530

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, send_file
+import yt_dlp
+import os
 
 app = Flask(__name__)
+
+DOWNLOAD_FOLDER = "downloads"
+os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/download", methods=["POST"])
-def download():
-    return jsonify({"status": "ok"})
+def download_video():
+    url = request.form["url"]
+
+    ydl_opts = {
+        "outtmpl": f"{DOWNLOAD_FOLDER}/%(title)s.%(ext)s",
+        "format": "best"
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+
+        return f"Downloaded: {filename}"
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
