2d3152ea5fc5382daa642acc963d11621350d14a
Sangeetha-123549 <sangeethacseaisvpcet@gmail.com>
2026-05-17 11:50:41 +0530

    app.py

--- app.py
+++ app.py
@@ -1,54 +1,14 @@
-from flask import Flask, render_template, request, send_file, jsonify
-import yt_dlp
-import os
-import time
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-
-DOWNLOAD_FOLDER = "downloads"
-os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/download", methods=["POST"])
 def download():
-    data = request.get_json()
-    url = data.get("url")
-
-    if not url:
-        return jsonify({"error": "No URL"}), 400
-
-    filename = f"video_{int(time.time())}.mp4"
-
-    ydl_opts = {
-        'outtmpl': f'{DOWNLOAD_FOLDER}/{filename}',
-        'format': 'best'
-    }
-
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-
-        return jsonify({"status": "ok"})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-@app.route("/file")
-def file():
-    files = sorted(os.listdir(DOWNLOAD_FOLDER), reverse=True)
-
-    if not files:
-        return "Video not found", 404
-
-    latest = files[0]
-    return send_file(os.path.join(DOWNLOAD_FOLDER, latest), as_attachment=True)
-
+    return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
